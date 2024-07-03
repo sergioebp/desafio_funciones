3a9f6c85fdaf57ca608a42bfe58aeb901d01d688
Sergio Baquero <sergioebp@gmail.com>
2024-07-03 14:43:47 -0400

    desafio completo, página teclas funcional

--- assets/js/teclas.js
+++ assets/js/teclas.js
@@ -1,24 +1,52 @@
-var color = 'white'
+var color = 'white';
 
-div = document.getElementById('key')
+div = document.getElementById('key');
 
 let cambio = function(x) {
     div.style.backgroundColor = x
+};
+
+let cuadrado = "height: 200px; width: 200px; border: solid black 5px";
+
+let crear = function(color) {
+    nuevodiv.style.width = "200px";
+    nuevodiv.style.height = "200px";
+    nuevodiv.style.border = "solid black 5px";
+    nuevodiv.style.backgroundColor = color;
 }
-
-
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "a" || event.key === "A") {
-        color = 'pink'
+        color = 'pink';
+        cambio(color)
     }
     else if (event.key === "s" || event.key === "S") {
-        color = 'orange'
+        color = 'orange';
+        cambio(color)
     }
     else if (event.key === "d" || event.key === "D") {
-        color = 'lightblue'
-    }
-    else if (event.key === "q" || event.key === "Q" || event.key === "w" || event.key === "W" || event.key === "e" || event.key === "E") {
+        color = 'lightblue';
         cambio(color)
+    }
+    else if (event.key === "q" || event.key === "Q") {
+        color = 'purple';
+        let nuevodiv = document.createElement("div");
+        nuevodiv.style = cuadrado
+        nuevodiv.style.backgroundColor = color;
+        document.body.appendChild(nuevodiv)
+    }
+    else if (event.key === "w" || event.key === "W") {
+        color = 'grey';
+        let nuevodiv = document.createElement("div");
+        nuevodiv.style = cuadrado
+        nuevodiv.style.backgroundColor = color;
+        document.body.appendChild(nuevodiv)
+    }
+    else if (event.key === "e" || event.key === "E") {
+        color = 'brown';
+        let nuevodiv = document.createElement("div");
+        nuevodiv.style = cuadrado
+        nuevodiv.style.backgroundColor = color;
+        document.body.appendChild(nuevodiv)
     }
 })
